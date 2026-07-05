e59c75b936444a7d0aa6384eabbb13cc892e992b
Ankita Sharma <shraddhaasharma1507@gmail.com>
2026-07-05 20:45:20 +0530

    solution of leetcode problem 234

---
+++ leetcodeproblem234.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> values = new ArrayList<>();
+
+        ListNode curr = head;
+        while (curr != null) {
+            values.add(curr.val);
+            curr = curr.next;
+        }
+
+        int left = 0;
+        int right = values.size() - 1;
+        while (left < right) {
+            if (!values.get(left).equals(values.get(right))) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
