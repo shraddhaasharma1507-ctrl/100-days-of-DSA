ccb77f6f9857bd688fa11e18617ad42e18095a54
Ankita Sharma <shraddhaasharma1507@gmail.com>
2026-07-07 22:03:06 +0530

    solution of problem leetcode 225

---
+++ leetcodeproblem225.java
@@ -0,0 +1,28 @@
+class MyStack {
+    private Queue<Integer> queue;
+
+    public MyStack() {
+        queue = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        queue.add(x);
+        int size = queue.size();
+        while (size > 1) {
+            queue.add(queue.poll());
+            size--;
+        }
+    }
+
+    public int pop() {
+        return queue.poll();
+    }
+
+    public int top() {
+        return queue.peek();
+    }
+
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
