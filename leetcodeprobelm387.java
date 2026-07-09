c8f8074c0c84f0344a5d0c2ba316fc9ea1f7f3f4
Ankita Sharma <shraddhaasharma1507@gmail.com>
2026-07-09 21:53:54 +0530

    solution of leetcode problem 387

---
+++ leetcodeprobelm387.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();    
+        for (int i=0; i<s.length(); i++){
+           char c =s.charAt(i);
+           if (map.containsKey(c)){
+            int oldcount = map.get(c);
+            map.put(c, oldcount+1);
+           }
+           else{
+            map.put(c, 1);
+           }
+       }
+
+       for (int i=0; i<s.length();i++){
+        if(map.get(s.charAt(i))==1){
+            return i;
+        }
+       }
+       return -1;
+    }
+}
